2129a6c834f23aab9687c689f4b2b0b27f96f73b
Mysyrf <masyrafpratama19@gmail.com>
2025-03-17 14:52:35 +0700

    2

---
+++ ex1_statment.dart
@@ -0,0 +1,9 @@
+//statment 
+void main (List<String>args){
+  print('Pemrograman Android');
+  print('Mengunakan Dart dan Flutter');
+  int a, b, c;
+  String strA = "A";
+  //apabila 2 statment digabungkan dalam satu baris maka akan terjadi error 
+  double d; int i;
+}
